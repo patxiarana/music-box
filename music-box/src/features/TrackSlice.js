db9c7b7d50222011d07fd5e41b36539d742ac339
patxi <patxiarana05@gmail.com>
2023-03-13 10:05:45 -0300

    configuring redux toolkit and install firebase

---
+++ music-box/src/features/TrackSlice.js
@@ -0,0 +1,16 @@
+features/counter/counterSlice.js
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: [],
+}
+
+export const TrackSlice = createSlice({
+    name:track,
+    initialState,
+    reducers:{
+        getTracks: (state) => {
+            
+        }
+    }
+})
