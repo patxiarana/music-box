194212714185938293b85487e0e600ee2423a86e
patxi <patxiarana05@gmail.com>
2023-03-13 10:49:06 -0300

    install and prove music player

--- music-box/src/App.jsx
+++ music-box/src/App.jsx
@@ -1,11 +1,14 @@
-
+import React from 'react'
 import './App.css'
-
+import { Player1 } from './components/player1'
 function App() {
 
 
   return (
+    <div>
  <h1>START PROYECT</h1>
+ <Player1/>
+ </div>
   )
 }
 
--- music-box/src/main.jsx
+++ music-box/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import './firebase.js'
+import "@madzadev/audio-player/dist/index.css";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Provider store={store}>
